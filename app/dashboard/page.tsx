7e829246a7567826c750a4a74a88803959f52919
Bugra Ozkan <bugraozkan@live.com>
2023-06-20 14:38:36 +0100

    Add ShareDashboardButton to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,18 @@
 "use client"
 
 import { useState, useEffect } from 'react';
-import { supabaseAdmin } from '@/utils/supabase-admin';
+import { supabaseAdmin, getUsername } from '@/utils/supabase-admin';
 import type { Metric } from '@/utils/supabase-admin';
 import MetricCard from './MetricCard';
 import MetricInsertCard from './MetricInsertCard';
+import ShareDashboardButton from './ShareDashboardButton';
 import { useSupabase } from '@/app/supabase-provider';
 
 const Dashboard = () => {
 
   const [metrics, setMetrics] = useState<Metric[]>([]);
   const [userId, setUserId] = useState<string | null | undefined>(null);
+  const [username, setUsername] = useState<string | null | undefined>(null);
   const { supabase } = useSupabase();
 
   const handleMetricAdded = (newMetric: Metric) => {
@@ -21,12 +23,20 @@ const Dashboard = () => {
     const fetchMetrics = async () => {
       try {
         const { data: { user } } = await supabase.auth.getUser();
-        setUserId(user?.id);
+
+        if (user?.id) {
+          const fetchedUsername = await getUsername(user.id);
+          if (typeof fetchedUsername === 'string') {
+            setUsername(fetchedUsername);
+          }
+          setUserId(user.id);
+        }
+    
         const { data, error } = await supabaseAdmin
           .from('metrics')
           .select('*')
           .eq('user_id', user?.id);
-          
+
         if (error) throw error;
         setMetrics(data || []);
       } catch (error) {
@@ -38,9 +48,9 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <section className="mb-32 bg-black">
+    <section className="mb-32 bg-black relative">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="max-w-6xl px-4 py-8 mx-auto sm:px-6 sm:pt-24 lg:px-8">
+        <div className="pt-8 mx-autosm:pt-24">
           <div className="sm:align-center sm:flex sm:flex-col">
             <h1 className="text-4xl font-extrabold text-white sm:text-center sm:text-6xl">
               Dashboard
@@ -49,6 +59,11 @@ const Dashboard = () => {
               We partnered with Stripe for a simplified billing.
             </p>
           </div>
+          {username && (
+            <div className="top-4 right-4">
+              <ShareDashboardButton username={username} />
+            </div>
+          )}
         </div>
         <div className="mt-4 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
           {metrics.map((metric) => (
